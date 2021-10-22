f95ddd2407dfee8718ca02b2c68bed0e107e732e
Rohan Bhangui <bhanguirohan@gmail.com>
2021-10-21 22:22:11 -0400

    updated the meta tags

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -156,18 +156,14 @@ const Home = () => {
             <h2 className="h1">Listen</h2>
             <div className="content">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
+                A reintroduction to the world of Sam Drysdale
+                following his 2019 debut "Vicelove". A new sonic
+                environment richer in major-driven pop sentiments
+                whilst maintaining focus on lyrical depth and growth.
               </p>
               <p>
-                Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt
-                in culpa qui officia deserunt mollit anim id est
-                laborum.
+              A more explorative production endeavor with styles
+                ranging in influence from bubblegum to blues.
               </p>
             </div>
           </div>
